f139fc02209a5e93d1c50d440698eb62e4165ebc
pragyakar <pragyakar75@gmail.com>
2019-12-23 17:20:16 +0545

    :hammer: Refactor button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 function Button(props) {
 
-  const { text, clickHandler } = props;
+  const { text, size, clickHandler } = props;
   
+  const sizeClass = (size = 'medium') => {
+    switch(size.toLowerCase()) {
+      case 'small':
+        return 'btn-sm'
+      case 'medium':
+        return 'btn-md'
+      case 'large':
+        return 'btn-lg'
+      default:
+        return 'btn-md'
+    }
+  }
+
   return (
-    <span className="btn-sp" onClick={clickHandler}>
+    <span 
+      className={`btn ${sizeClass(size)}`} 
+      onClick={clickHandler}
+    >
       {text}
     </span>
   )
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -7,7 +7,7 @@ function Landing() {
       <div className="landing-content">
         <p className="title-text">The Future of Learning</p>
         <p className="subtitle-text">We help you build yourself for the future that you deserve</p>
-        <Button text={'Learn More'} clickHandler={() => {}} />
+        <Button text={'Learn More'} size={'large'} clickHandler={() => {}} />
       </div>
       <img src="images/svg/bottom.svg" className="img-bottom" alt="footer-svg" />
     </div>
