866c8752d2961968d18020d7ba63277740497a4d
pragyakar <pragyakar75@gmail.com>
2019-12-23 14:39:33 +0545

    :bug: Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-	<h1>Apprentice</h1>
+	    <h1>Apprentice</h1>
     </div>
   );
 }
